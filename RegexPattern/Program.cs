8df122b623dc889214d14b12e075d73185b3f563
charasullia <charanrajr713@gmail.com>
2021-10-08 17:40:20 +0530

    [charansullia]added:Validating given all email samples

--- RegexPattern/Program.cs
+++ RegexPattern/Program.cs
@@ -6,8 +6,19 @@ namespace RegexPattern
     {
         static void Main(string[] args)
         {
-           RegexPattern pattern = new RegexPattern();
-            Console.WriteLine(pattern.ValidatePattern("Mangalore@123"));
+            RegexPattern pattern = new RegexPattern();
+            Console.WriteLine(pattern.ValidatePattern("abc.xyz@bl.co.in"));
+            Console.WriteLine(pattern.ValidatePattern("abc-100@yahoo.com"));
+            Console.WriteLine(pattern.ValidatePattern("abc..2002@gmail.com"));
+            Console.WriteLine(pattern.ValidatePattern("abc()*@gmail.com"));
+            Console.WriteLine(pattern.ValidatePattern("abc+100@gmail.com"));
+            Console.WriteLine(pattern.ValidatePattern("abc@1.com"));
+            Console.WriteLine(pattern.ValidatePattern("abc.100@abc.com.au"));
+            Console.WriteLine(pattern.ValidatePattern("abc111@abc.com"));
+            Console.WriteLine(pattern.ValidatePattern("abc-100@abc.net"));
+            Console.WriteLine(pattern.ValidatePattern("abc@gmail.com.1a"));
+            Console.WriteLine(pattern.ValidatePattern("abc@%*.com"));
+            Console.WriteLine(pattern.ValidatePattern("abc@abc@gmail.com"));
             Console.ReadKey();
         }
     }
--- RegexPattern/RegexPattern.cs
+++ RegexPattern/RegexPattern.cs
@@ -7,7 +7,7 @@ namespace RegexPattern
 {
     class RegexPattern
     {
-        public static string Regex_Pattern = "^([A-Za-z0-9])*([/+-@_&$#])?([A-Za-z0-9])*$";
+        public static string Regex_Pattern = "^[a-zA-Z0-9]+([.#_$+-][a-zA-Z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2})?$";
 
         public bool ValidatePattern(string Pattern)
         {
